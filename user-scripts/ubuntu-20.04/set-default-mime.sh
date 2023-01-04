b3a0f14b2f78e63046b4777fdbfe1039b827ddbd
NunoSempere <nuno.sempere@protonmail.com>
2023-01-04 15:42:08 +0100

    tweak: allow rose.desktop to be used as default mime type
    and add instructions to do so.

---
+++ user-scripts/ubuntu-20.04/set-default-mime.sh
@@ -0,0 +1,8 @@
+# Requires:
+# sudo cp rose.desktop /usr/share/applications
+
+xdg-mime default rose.desktop x-scheme-handler/https
+xdg-mime default rose.desktop x-scheme-handler/http
+
+# This is useful e.g., for setting how to open
+# links in vim when pressing gx.
