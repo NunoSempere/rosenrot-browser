aec21c71a65e68ac8488271a94fbf23f4f894f0d
NunoSempere <nuno.sempere@protonmail.com>
2023-08-03 14:40:40 +0200

    tweaks: add code to block twitter ads

--- plugins/style/style.h
+++ plugins/style/style.h
@@ -1,7 +1,7 @@
 #ifndef STYLE
 #define STYLE
 
-#define STYLE_N 4390 + 100
+#define STYLE_N 4748 + 100
 
 void read_style_js(char* string);
 
--- plugins/style/style.js
+++ plugins/style/style.js
@@ -61,9 +61,20 @@ if (document.domain == "search.nunosempere.com" ){
 }
 if (document.domain == "twitter.com" ){
   styles = `
+	/* hide promoted tweets */
+	:has(meta[property="og:site_name"][content="Twitter"])
+		[data-testid="cellInnerDiv"]:has(svg + [dir="auto"]) {
+		display: none;
+	}
 	[data-testid^="placementTracking"] {
 		display: none;
 	}
+
+  /* hide what's happening section */
+  :has(meta[property="og:site_name"][content="Twitter"])
+    [aria-label="Timeline: Trending now"] {
+    display: none !important;
+  }
 	[data-testid^="sidebarColumn"] {
 		display: none;
 	}
@@ -79,6 +90,7 @@ if (document.domain == "reddit.com" || document.domain == "old.reddit.com"){
 	.infobar-toaster-container,
 	#eu-cookie-policy,
 	.ad-container,
+	.listingsignupbar,
 	a[href="/premium"],
 	[data-promoted^="true"],
 	a[href^="https://alb.reddit.com"]
