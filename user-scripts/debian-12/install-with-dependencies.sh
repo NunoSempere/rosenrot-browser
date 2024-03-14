0f2ca9b9f7790a664a548bc6034711a17101519d
NunoSempere <nuno.semperelh@protonmail.com>
2024-03-14 15:59:51 -0300

    document how to add a shortcut in Debian

--- user-scripts/debian-12/install-with-dependencies.sh
+++ user-scripts/debian-12/install-with-dependencies.sh
@@ -23,3 +23,18 @@ cd  -
 # Debian desktop icon
 chmod +x rosenrot.desktop
 sudo cp rosenrot.desktop /usr/share/applications
+
+# Optionally, set a shortcut (within GNOME)
+# https://askubuntu.com/questions/597395/how-to-set-custom-keyboard-shortcuts-from-terminal/1007035#1007035
+name="rose"
+binding="<CTRL><SHIFT>W"
+action="/usr/bin/rosenrot"
+
+media_keys=org.gnome.settings-daemon.plugins.media-keys
+custom_kbd=org.gnome.settings-daemon.plugins.media-keys.custom-keybinding
+kbd_path=/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/$name/
+new_bindings=`gsettings get $media_keys custom-keybindings | sed -e"s>'\]>','$kbd_path']>"| sed -e"s>@as \[\]>['$kbd_path']>"`
+gsettings set $media_keys custom-keybindings "$new_bindings"
+gsettings set $custom_kbd:$kbd_path name "$name"
+gsettings set $custom_kbd:$kbd_path binding "$binding"
+gsettings set $custom_kbd:$kbd_path command "$action"
